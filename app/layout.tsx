92b2f46d147c2b012ae5333be0889848233afac8
Thijn <thijnkroon@gmail.com>
2025-10-14 09:13:51 +0200

    chore: disable custom cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Nav from "@/app/nav";
 import SocialLinks from "@/app/social-links";
-import { CursorWrapper } from "@/components/custom-cursor";
 import NoiseOverlay from "@/components/noise-overlay";
 import ThemeToggle from "@/components/theme-toggle";
 
@@ -53,17 +52,15 @@ export default function RootLayout({
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <CursorWrapper>
-            <NoiseOverlay />
-            <div className="relative flex min-h-screen xs:w-fit xs:flex-row flex-col p-6 font-sans text-sm leading-6 sm:p-10 sm:text-[15.4px] md:p-20">
-              <Nav />
-              <main className="relative flex h-fit max-w-2xl xs:flex-row flex-col">
-                <SocialLinks className="xs:mx-7 my-4 xs:my-0 sm:mx-10 md:mx-12" />
-                <div className="relative h-fit">{children}</div>
-              </main>
-              <ThemeToggle className="absolute right-6 xs:translate-y-0.5 transform sm:right-10 md:right-20" />
-            </div>
-          </CursorWrapper>
+          <NoiseOverlay />
+          <div className="relative flex min-h-screen xs:w-fit xs:flex-row flex-col p-6 font-sans text-sm leading-6 sm:p-10 sm:text-[15.4px] md:p-20">
+            <Nav />
+            <main className="relative flex h-fit max-w-2xl xs:flex-row flex-col">
+              <SocialLinks className="xs:mx-7 my-4 xs:my-0 sm:mx-10 md:mx-12" />
+              <div className="relative h-fit">{children}</div>
+            </main>
+            <ThemeToggle className="absolute right-6 xs:translate-y-0.5 transform sm:right-10 md:right-20" />
+          </div>
         </ThemeProvider>
       </body>
     </html>
