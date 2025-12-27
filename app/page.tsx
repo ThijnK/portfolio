48cf691fb339542d298bd2f92d3515ba4579eeb7
Thijn <thijnkroon@gmail.com>
2025-12-27 15:35:11 +0100

    chore: update intro

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <Article title="Thijn Kroon">
       <Section>
         <Paragraph>
-          Hi, I’m Thijn—a software developer who enjoys working across the
-          stack. I take pride in writing code that’s not just functional, but
-          clean, organized, and well-structured. Outside of coding, I like to
-          run, read, and occasionally lose track of time gaming.
+          Hi, I’m Thijn—a software engineer who enjoys working across the stack.
+          I take pride in writing code that’s not just functional, but clean,
+          organized, and well-structured. Outside of coding, I like to run,
+          read, and occasionally lose track of time gaming.
         </Paragraph>
         <Paragraph>
           I graduated from Utrecht University with both a bachelor’s and
